097a25fe9247123191a362cb1d6060db4a049257
McManus <macmanuch@hotmail.fr>
2020-03-31 00:30:18 -0400

    add experience component

---
+++ src/components/experience/experience.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from './experience.module.scss'
+
+const Experience = props => {
+  return (
+    <div>
+      <span>test</span>
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/experience/experiences.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import style from './experiences.module.scss'
+
+import Separator from '../separator/separator'
+import Experience from './experience'
+
+const Experiences = props => {
+  console.log(props)
+  return (
+    <section data-section="experiences">
+      <div className={style.experiences}>
+        <h2>Experiences</h2>
+
+        <Separator></Separator>
+        {props.data.map((item, index) => {
+          return <Experience data={item.node} key={index}></Experience>
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Experiences
