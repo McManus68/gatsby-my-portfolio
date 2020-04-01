716e419b572d9ab0d93e272610168afad1b3a8e8
McManus <macmanuch@hotmail.fr>
2020-04-01 13:34:27 -0400

    handle react-on-visible to handle animation on Viewport

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import style from './experience.module.scss'
+
+import OnVisible from 'react-on-visible'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Experience = props => {
   return (
     <div className={style.experience}>
-      <div className={style.icon}>
-        <FontAwesomeIcon icon={props.data.icon} size="2x" />
-      </div>
+      <OnVisible visibleClassName={style.animateIcon} className={style.icon}>
+        <FontAwesomeIcon icon={props.data.icon} />
+      </OnVisible>
 
-      <div className={style.content}>
-        <div className={style.date}>{props.data.period}</div>
-        <h4>{props.data.title}</h4>
-        <span
-          dangerouslySetInnerHTML={{
-            __html: props.data.description.childMarkdownRemark.html,
-          }}
-        />
-      </div>
+      <OnVisible visibleClassName={style.animateContent} className="hidden">
+        <div className={style.content}>
+          <div className={style.date}>{props.data.period}</div>
+          <h4>{props.data.title}</h4>
+          <span
+            dangerouslySetInnerHTML={{
+              __html: props.data.description.childMarkdownRemark.html,
+            }}
+          />
+        </div>
+      </OnVisible>
     </div>
   )
 }
