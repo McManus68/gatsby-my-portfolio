df5518345e986ace5342721a8a83a7744370b04b
McManus <macmanuch@hotmail.fr>
2020-04-02 23:56:36 -0400

    contact is a func component

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,58 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import style from './contact.module.scss'
 
 import Button from '../button/button'
 import Section from '../section/section'
+import DiamondIcon from '../diamond-icon/diamond-icon'
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props)
-    this.submitForm = this.submitForm.bind(this)
-    this.state = {
-      status: '',
-    }
-  }
+const Contact = props => {
+  let [status, setStatus] = useState({
+    status: '',
+  })
 
-  render() {
-    const { status } = this.state
-    return (
-      <section data-section="contact" className={style.contact}>
-        <Section title="Contact">
-          <form
-            className={style.form}
-            onSubmit={this.submitForm}
-            action="https://formspree.io/xgelendk"
-            method="POST"
-          >
-            <label> Votre nom: </label>
-            <span className={style.formItem}>
-              <input type="text" name="name" />
-            </span>
-
-            <label> Votre adresse de messagerie: </label>
-            <span className={style.formItem}>
-              <input type="email" name="email" />
-            </span>
-
-            <label> Votre message: </label>
-            <span className={style.formItem + ' ' + style.textarea}>
-              <textarea name="message" />
-            </span>
-
-            <p className={style.buttonContainer}>
-              <Button label="Envoyer"></Button>
-            </p>
-
-            {status === 'SUCCESS' && <p>Thanks!</p>}
-            {status === 'ERROR' && <p>Ooops! There was an error.</p>}
-          </form>
-        </Section>
-      </section>
-    )
-  }
-
-  submitForm(ev) {
+  let submitForm = ev => {
     console.log('pwet')
     ev.preventDefault()
     const form = ev.target
@@ -64,11 +23,48 @@ export default class Contact extends React.Component {
       if (xhr.readyState !== XMLHttpRequest.DONE) return
       if (xhr.status === 200) {
         form.reset()
-        this.setState({ status: 'SUCCESS' })
+        setStatus('SUCCESS')
       } else {
-        this.setState({ status: 'ERROR' })
+        setStatus('ERROR')
       }
     }
     xhr.send(data)
   }
+
+  return (
+    <section data-section="contact" className={style.contact}>
+      <Section title="Contact">
+        <form
+          className={style.form}
+          onSubmit={submitForm}
+          action="https://formspree.io/xgelendk"
+          method="POST"
+        >
+          <label> Votre nom: </label>
+          <span className={style.formItem}>
+            <input type="text" name="name" />
+          </span>
+
+          <label> Votre adresse de messagerie: </label>
+          <span className={style.formItem}>
+            <input type="email" name="email" />
+          </span>
+
+          <label> Votre message: </label>
+          <span className={style.formItem + ' ' + style.textarea}>
+            <textarea name="message" />
+          </span>
+
+          <p className={style.buttonContainer}>
+            <Button label="Envoyer"></Button>
+          </p>
+
+          {status === 'SUCCESS' && <p>Thanks!</p>}
+          {status === 'ERROR' && <p>Ooops! There was an error.</p>}
+        </form>
+      </Section>
+    </section>
+  )
 }
+
+export default Contact
