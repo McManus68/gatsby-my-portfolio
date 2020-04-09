0b832c0619f0f0c80e1c7e534d7f7412bf19a565
McManus <macmanuch@hotmail.fr>
2020-04-09 19:07:06 -0400

    Add contact image BG

--- src/components/section/contact/contact.js
+++ src/components/section/contact/contact.js
@@ -3,13 +3,23 @@ import React from 'react'
 import style from './contact.module.scss'
 
 import ContactForm from './contact-form'
-import Section from '../../ui/section/section'
+import { Parallax } from 'react-parallax'
+
+import Separator from '../../ui/separator/separator'
+
+import bg from '../../../assets/images/contact.jpg'
 
 const Contact = props => {
   return (
-    <Section section={props.section} className={style.contact}>
-      <ContactForm />
-    </Section>
+    <section data-section={props.section.name} id={props.section.name}>
+      <Parallax bgImage={bg} className={style.background} strength={200}>
+        <div className={style.contact}>
+          <h2>{props.section.title}</h2>
+          <Separator />
+          <ContactForm />
+        </div>
+      </Parallax>
+    </section>
   )
 }
 
