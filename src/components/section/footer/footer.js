788a16375a6c7f9c9c5c3d04063540657d4c9799
McManus <macmanuch@hotmail.fr>
2020-04-09 20:54:40 -0400

    change footer / fix minor bugs

--- src/components/section/footer/footer.js
+++ src/components/section/footer/footer.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import style from './footer.module.scss'
 
+import { useTranslation } from 'react-i18next'
+
 const Footer = props => {
+  const { t } = useTranslation()
+
   return (
     <div className={style.footer} id="footer">
       <div className="container">
-        <span>{props.me.name}</span>
+        <span>{t('footer.copyright', { who: props.me.name })}</span>
       </div>
     </div>
   )
