d1315e200bcec3aa2ad291b4b4ee49785da7c380
McManus <macmanuch@hotmail.fr>
2020-04-10 17:06:55 -0400

    change global style

--- src/components/section/skill/skill-group.js
+++ src/components/section/skill/skill-group.js
@@ -5,6 +5,8 @@ import style from './skill-group.module.scss'
 import SkillItem from './skill-item'
 
 const SkillGroup = props => {
+  const handleOnDragStart = e => e.preventDefault()
+
   return (
     <div className={style.skillGroup}>
       {props.data.map((item, index) => {
