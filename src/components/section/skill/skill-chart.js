e88782891414c1747debf6060fbcd7a0c5048a7b
McManus <macmanuch@hotmail.fr>
2020-04-17 21:12:46 -0400

    skills graph beta

--- src/components/section/skill/skill-chart.js
+++ src/components/section/skill/skill-chart.js
@@ -4,6 +4,8 @@ import * as d3 from 'd3'
 
 import style from './skill-chart.module.scss'
 
+import avatar from '../../../assets/images/avatar.png'
+
 class SkillChart extends Component {
   componentDidMount() {
     this.drawChart()
@@ -32,8 +34,6 @@ class SkillChart extends Component {
       type: 'root',
     })
 
-    console.log('nodes = ', graphNodes)
-
     // Create links
     let graphLinks = []
 
@@ -46,14 +46,11 @@ class SkillChart extends Component {
       graphLinks.push({ source: 0, target: rootIndex * 1 + 1 })
     })
 
-    console.log('links = ', graphLinks)
-
     // Draw Graph
     var width = 800,
       height = 600
 
     var svg = d3.select(this.refs.chart).append('svg').attr('width', width).attr('height', height)
-
     var radius = d3.scaleSqrt().domain([0, 50]).range([0, 20])
     var drag = d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended)
 
@@ -68,12 +65,38 @@ class SkillChart extends Component {
     // Root node for Links and Nodes
     var link = svg.append('g').attr('class', 'link').selectAll('line')
     var node = svg.append('g').attr('class', 'node').selectAll('circle')
+    var tooltip = svg.append('div').style('opacity', 0).attr('class', 'tooltip')
 
     link = link.data(graphLinks).enter().append('line')
 
     var node = node.data(graphNodes).enter().append('g').call(drag)
 
+    // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
+    // Its opacity is set to 0: we don't see it by default.
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    var mouseover = function (d) {
+      tooltip.style('opacity', 1)
+    }
+
+    var mousemove = function (d) {
+      tooltip
+        .html('The exact value of<br>the Ground Living area is: ' + d.GrLivArea)
+        .style('left', d3.mouse(this)[0] + 90 + 'px') // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+        .style('top', d3.mouse(this)[1] + 'px')
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    var mouseleave = function (d) {
+      tooltip.transition().duration(200).style('opacity', 0)
+    }
+
+    // Draw all nodes excepts the root node
     node
+      .filter(function (d) {
+        return d.type !== 'root'
+      })
       .append('circle')
       .attr('r', function (d) {
         return radius(d.level * 20)
@@ -88,15 +111,39 @@ class SkillChart extends Component {
             return d.type
         }
       })
+      .on('mouseover', mouseover)
+      .on('mousemove', mousemove)
+      .on('mouseleave', mouseleave)
 
-    /*node
+    // Draw the root node
+    node
+      .filter(function (d) {
+        return d.type === 'root'
+      })
+      .append('image')
+      .attr('class', 'avatar')
+      .attr('x', '-32')
+      .attr('y', '-32')
+      .attr('height', '64')
+      .attr('width', '64')
+      .attr('href', avatar)
+
+    // Add the text for the groups nodes
+    node
+      .filter(function (d) {
+        return d.type === 'group'
+      })
       .append('text')
+      .attr('class', function (d) {
+        return d.name
+      })
       .attr('dy', '.35em')
       .attr('text-anchor', 'middle')
       .text(function (d) {
         return d.name
-      })*/
+      })
 
+    // Add the icon for the technos nodes
     node
       .filter(function (d) {
         return d.logo
