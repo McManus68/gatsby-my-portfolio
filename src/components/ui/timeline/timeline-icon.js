8e52c6b8dfd9bd47e0ee57c9ff7c0ee9507a6193
McManus <macmanuch@hotmail.fr>
2020-04-21 00:13:25 -0400

    remove bounceIn anim on timeline icon

--- src/components/ui/timeline/timeline-icon.js
+++ src/components/ui/timeline/timeline-icon.js
@@ -2,17 +2,13 @@ import React from 'react'
 
 import style from './timeline-icon.module.scss'
 
-import OnVisible from 'react-on-visible'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const TimelineIcon = props => {
+const TimelineIcon = (props) => {
   return (
-    <OnVisible
-      visibleClassName={style.bounceIn}
-      className={`${style.timelineIcon} ${props.active ? style.active : ''}`}
-    >
+    <div className={`${style.timelineIcon} ${props.active ? style.active : ''}`}>
       <FontAwesomeIcon icon={props.icon} />
-    </OnVisible>
+    </div>
   )
 }
 
